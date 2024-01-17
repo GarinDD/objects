066bfe8da06d47922f8fb17b3795a9eee93109ce
Daniel` <streetbossog@gmail.com`>
2024-01-17 19:45:13 +0300

    done`

---
+++ homework/jS_Homework-main/jS_Homework-main/object.js
@@ -0,0 +1,138 @@
+
+
+
+
+
+
+// 15. Создайте объект "город" с полями "название", "население" и "страна".
+
+// // 1. Создайте объект "автомобиль" с полями "марка", "год выпуска" и "цвет".
+// 2. Добавьте метод "завести двигатель" к объекту "автомобиль".
+// let car = {
+//     model : `Subaru impeza`,
+//     releaseYear : 1992,
+//     color : `silver`,
+// }
+
+// function startEngine() {
+//     alert(`Switch it up, driver!`);
+// }
+// car.startEngine = startEngine;
+// car.startEngine();
+
+// // 3. Создайте объект "пользователь" с полями "имя", "возраст" и "адрес".
+// 4. Измените значение поля "возраст" в объекте "пользователь".
+// let user = {
+//     name : `Nina`,
+//     age : 26,
+//     address : `Bald mountain 22/21`,
+// }
+
+// let age = user.age;
+// age = 22;
+
+// 5. Создайте объект "книга" с полями "название", "автор" и "год издания".
+// 6. Выведите на экран информацию о книге, используя поля объекта "книга".
+
+// let book = {
+//     name : `How to quit smoking`,
+//     author : `Alen Karr`,
+//     yearReleased : 1550,
+// }
+
+// nameX.innerText = book.name
+// authorX.innerText = book.author
+// yearReleasedX.innerText = book.yearReleased
+
+// 7. Создайте объект "животное" с методом "издать звук".
+// 8. Измените значение поля "цвет" в объекте "автомобиль".
+
+// let animal = {
+//     species : `feline`,
+//     habitat : `household,forest`,
+//     population : 100000,
+// }
+
+// animal.makeNoise = function noise() {
+//     alert(`Meow`);
+// }
+
+// animal.makeNoise();
+
+
+
+
+// 9. Создайте объект "продукт" с полями "название", "цена" и "количество".
+// 10. Увеличьте количество продукта в объекте "продукт" на 5.
+
+
+
+// let product = {
+//     name : `phone`,
+//     price : 1000,
+//     amount : 5,
+// }
+
+// product.increase = function upBy5() {
+//     product.amount += 5;
+//     alert(`The amount of ${product.name} has been increased by ${product.amount} `)
+// }
+
+// product.increase();
+
+
+// 11. Создайте объект "компьютер" с методом "включить".
+// 12. Измените значение поля "адрес" в объекте "пользователь".
+
+// let computer = {
+//     model : `i7`,
+//     processor : `intel`,
+//     ssd : 500,
+//     power : `off`,
+// }
+
+// computer.turnOn = function turnOn () {
+//     computer.power = `on`;
+//     alert(`Your computer has been turned ${computer.power}`)
+// }
+// computer.turnOn();
+
+
+// 13. Создайте объект "фрукт" с методом "почистить".
+
+// let fruit = {
+//     name : `apple`,
+//     color : `yellow`,
+//     vitamins : `A,B,C`,
+//     weight : 1000,
+// }
+
+// fruit.peel = function peel() {
+//     fruit.weight -= 50;
+//     alert(`You have peeled the fruit`);
+// }
+// fruit.peel();
+
+
+
+// // 14. Выведите на экран только имена всех полей объекта "пользователь".
+
+// let users = {
+//     admin : `Mihail`,
+//     moderator : `Kostya`,
+//     owner : `Daniil`,
+//     member : `Borya`,
+// }
+
+// adminX.innerText = users.admin
+// moderatorX.innerText = users.moderator
+// ownerX.innerText = users.owner
+// memberX.innerText = users.member
+
+// 15. Создайте объект "город" с полями "название", "население" и "страна".
+
+// let city = {
+//     name : `Sochi`,
+//     population : 1500000,
+//     country : `Russian Federation`,
+// }
